52fb00003b67932ce07bc39adaef130768201338
Erich Wang <eriwan@microsoft.com>
2017-03-15 14:26:16 +0800

    initial check in project template for azure iot hub sample

---
+++ AzureIoTHubSampleTemplate/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace $safeprojectname$
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CancellationTokenSource tokenSource = new CancellationTokenSource();
+
+            Console.CancelKeyPress += (s, e) =>
+            {
+                e.Cancel = true;
+                tokenSource.Cancel();
+                Console.WriteLine("Existing ...");
+            };
+            Console.WriteLine("Press CTRL+C to exit");
+
+            Task.WaitAll(
+                AzureIoTHub.SendDeviceToCloudMessageAsync(tokenSource.Token),
+                AzureIoTHub.ReceiveMessagesFromDeviceAsync(tokenSource.Token)
+                );
+        }
+    }
+}
