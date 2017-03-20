79f2c80103f39f6ec75c913cfb1d687cdb9d7079
rentu <rentu@microsoft.com>
2017-03-20 10:42:09 +0800

    Add wizard for connection string when create project

---
+++ AzureIoTHubSampleVsix/ConnectionStringWizard.cs
@@ -0,0 +1,50 @@
+﻿using EnvDTE;
+using Microsoft.VisualStudio.TemplateWizard;
+using System.Collections.Generic;
+
+namespace AzureIoTHubSampleVsix
+{
+    class ConnectionStringWizard:IWizard
+    {
+        public void BeforeOpeningFile(ProjectItem projectItem)
+        {
+        }
+
+        public void ProjectFinishedGenerating(Project project)
+        {
+        }
+
+        public void ProjectItemFinishedGenerating(ProjectItem projectItem)
+        {
+        }
+
+        public void RunFinished()
+        {
+        }
+
+        public void RunStarted(object automationObject, Dictionary<string, string> replacementsDictionary, WizardRunKind runKind, object[] customParams)
+        {
+
+            var dialog=new ConnectionStringDialog();
+            var dialogCompleted = dialog.ShowModal();
+
+            if (dialogCompleted == true)
+            {
+                replacementsDictionary.Add("$ioTHubConnectionString$", dialog.IoTHubConnectionString.Text);
+                replacementsDictionary.Add("$deviceConnectionString$", dialog.DeviceConnectionString.Text);
+            }
+            else
+            {
+                replacementsDictionary.Add("$ioTHubConnectionString$", "HostName=$iotHubUri$;SharedAccessKeyName=$accessKeyName$;SharedAccessKey=$accessKey$");
+                replacementsDictionary.Add("$deviceConnectionString$", "HostName=$iotHubUri$;DeviceId=$deviceId$;SharedAccessKey=$deviceKey$");
+            }
+        }
+
+        public bool ShouldAddProjectItem(string filePath)
+        {
+            return true;
+        }
+
+    }
+}
+
